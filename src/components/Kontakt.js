2fc34939dc2e713bd6442e15210e98b0715817c5
MeiWR <94839136+MeiWR@users.noreply.github.com>
2024-07-18 19:33:53 +0200

    Add files via upload

--- src/components/Kontakt.js
+++ src/components/Kontakt.js
@@ -8,6 +8,7 @@ function Kontakt(){
                 <p>Brno</p>               
                 <p className="downloadCV"><a href="./download\CV_Tomas_Malcher.pdf" target="_blank" rel="noopener noreferrer">Stáhnout CV</a></p>
                 <p className="Copyright">@ Tomáš Malcher 2024</p>
+                <div className="EmptySpace"></div>
             </div>
         </div>
     )
--- src/components/PortVideo.js
+++ src/components/PortVideo.js
@@ -4,6 +4,7 @@ import React from "react";
 function PortVideo(){
     return (
         <div className="PortVideo">
+            <h4 className="videoHeading">Projekt do předmětu Filmová tvorba na Janáčkově akademii můzických umění.</h4>
             <YoutubeEmbed embedId="4HATR9TAmvM?si=olmGg8KYkeFlMBuW" />
         </div>
     )
