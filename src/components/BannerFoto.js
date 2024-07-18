cbcdfd388be170582ad0ac7cf688d511ded62be2
MeiWR <94839136+MeiWR@users.noreply.github.com>
2024-07-18 13:45:31 +0200

    Add files via upload

--- src/components/BannerFoto.js
+++ src/components/BannerFoto.js
@@ -3,7 +3,7 @@ import Typewriter from 'typewriter-effect';
 function BannerFoto(){
     return (
         <div className="Banner">
-            <img src="./imgs/home-bg.jpg" alt='Já.'/>
+            <img className='BannerFotka' src="./imgs/home-bg.jpg" alt='Já.'/>
             <div className='AboutWrap'>
                 <p className='AboutMeText'>Jsem studentem magisterského programu Audioinženýrství na VUT v Brně. Mimo zvuku a hudby mě baví cokoliv kreativního, od mala jsem si hrál s Photoshopem a podobnými programy. V posledních letech jsem v oblasti grafiky získal zkušenosti při kresbě a designu motivů oblečení a nebo například při zajištění grafické stránky největší vzpěračské soutěže na území ČR v roce 2023 - Czech Weightlifting Open.  Kromě grafiky mě baví focení, práce s videem. Ve volném čase se také věnuji výrobě elektrických kytar.
                 </p>
